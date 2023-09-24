2c6a4093baa856f0a6ac073beb63864a8d50f6d4
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-24 12:01:25 -0700

    add fetch() with api

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Global variables
+const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=chicken`;
+// Fetch Request
 
+fetch(URL)
+  .then((response) => response.json())
+  .then((foodObj) => console.log(foodObj))
+  .catch((error) => console.log("There was an error"));
 // Helper functions
 
-// Fetch Request
+// Render images
+
+// User form input
+
+// submit button
