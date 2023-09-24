10477b0b6ba883e6dd4ece5a46c9c6d0b2632b41
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-24 15:08:32 -0700

    fetched api, + renderFood function

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // Global variables
-const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=chicken`;
+const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=tacos`;
 // Fetch Request
-
 fetch(URL)
   .then((response) => response.json())
-  .then((foodObj) => console.log(foodObj))
+  .then((foodObj) => renderFood(foodObj))
   .catch((error) => console.log("There was an error"));
+
 // Helper functions
+
+const renderFood = (foodObj) => {
+  console.log(foodObj);
+  foodObj.results.forEach((food) => {
+    const foodName = food.title;
+    const foodImg = food.image;
+    console.log(foodName);
+    console.log(foodImg);
+  });
+};
 
 // Render images
 
