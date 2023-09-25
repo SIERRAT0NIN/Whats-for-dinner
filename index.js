179a591cd87eeb654700d3da30aa59cf8d6c45b4
Anne Hastings <anne.miriam@gmail.com>
2023-09-25 14:02:05 -0600

    updated html and started submit eventListener

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 // Global variables
+const form = document.querySelector('#new-form');
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+const submittedMeal = document.querySelector('#submitted-meal-container')
 
 // Helper functions
 
 // Fetch Request
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const mealData = mealData(form);
+    const mealObj = {};
+    mealData.forEach((value, key) => {
+        mealObj[key] = value;
+    })
+    sendMealToAPI(mealObj)
+});
+const sendMealToAPI = (mealObj => {
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(mealObj),
+    });
+    .then(resp => resp.json());
+    .then(mealArr = )
+})
