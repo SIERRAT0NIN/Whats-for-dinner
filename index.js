bc198d3079886821ba8fd728fe3041b3c55f2d5b
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-25 12:54:07 -0700

    add food img and selectedCountry btn text

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 // Global variables
-// const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=tacos`;
-const apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+
+const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?a=American`;
 const foodItems = document.querySelector(".food-items");
 const foodImgDisplay = document.querySelector("#food-img");
 const img = document.createElement("img");
 const dropdownItem = document.querySelector(".dropdown-item");
+const dropdownMenu = document.querySelector(".dropdown-menu");
 const dropdownLi = document.createElement("li");
+const country = document.querySelector(".country");
 
 // Fetch Request
 fetch(apiUrl)
   .then((response) => response.json())
-  .then((foodObj) => console.log(foodObj))
+  .then((mealData) => renderMeals(mealData))
   .catch((error) => console.log("There was an error"));
 
 // Helper functions
+// When a country is selected, update the selected-country text content with country's name
 
-// const renderFood = (foodObj) => {
-//   console.log(foodObj);
-//   foodObj.results.forEach((food) => {
-//     const foodImg = food.image;
-//     img.setAttribute("id", "foodImg");
-//     img.src = foodImg;
-//     foodImgDisplay.appendChild(img);
-//   });
-//   renderFoodList(foodObj);
-// };
+dropdownMenu.addEventListener("click", (e) => {
+  const selectedCountry = e.target.text;
+  country.textContent = selectedCountry;
+  console.log(selectedCountry);
+  return selectedCountry;
+});
+// Render img
 
-// const renderFoodList = (foodObj) => {
-//   foodObj.results.forEach((foodTitle) => {
-//     const foodName = foodTitle.title;
-//   });
-// };
+const renderMeals = (mealData) => {
+  mealData.meals.forEach((mealImg) => {
+    const mealImgUrl = mealImg.strMealThumb;
+    img.src = mealImgUrl;
+    foodImgDisplay.append(img);
 
-// Render food names to dropdown btns
+    console.log(mealImg);
+  });
+};
+
+// Selected country should be updated into the api using `${country} ` at the end of the api
+
+// Selected country's meals should be displayed into the drop down menu.
+
+// Use the .filter() to filter the sides and dessert. Will need another api for beverages.
 
 // User form input
 
