173879c55bca08c8c7a01d98f5fd4a4a66a57643
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-25 11:06:57 -0700

    add country btn with country names listed. New api being used.

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 // Global variables
-const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=tacos`;
+// const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=tacos`;
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
 const foodItems = document.querySelector(".food-items");
 const foodImgDisplay = document.querySelector("#food-img");
 const img = document.createElement("img");
 const dropdownItem = document.querySelector(".dropdown-item");
+const dropdownLi = document.createElement("li");
+
 // Fetch Request
-fetch(URL)
+fetch(apiUrl)
   .then((response) => response.json())
-  .then((foodObj) => renderFood(foodObj))
+  .then((foodObj) => console.log(foodObj))
   .catch((error) => console.log("There was an error"));
 
 // Helper functions
 
-const renderFood = (foodObj) => {
-  console.log(foodObj);
-  foodObj.results.forEach((food) => {
-    const foodImg = food.image;
-    img.setAttribute("id", "foodImg");
-    img.src = foodImg;
-    foodImgDisplay.appendChild(img);
-  });
-  renderFoodList(foodObj);
-};
-const renderFoodList = (foodObj) => {
-  foodObj.results.forEach((foodTitle) => {
-    const foodName = foodTitle.title;
+// const renderFood = (foodObj) => {
+//   console.log(foodObj);
+//   foodObj.results.forEach((food) => {
+//     const foodImg = food.image;
+//     img.setAttribute("id", "foodImg");
+//     img.src = foodImg;
+//     foodImgDisplay.appendChild(img);
+//   });
+//   renderFoodList(foodObj);
+// };
 
-    dropdownItem.append(foodName);
-  });
-};
+// const renderFoodList = (foodObj) => {
+//   foodObj.results.forEach((foodTitle) => {
+//     const foodName = foodTitle.title;
+//   });
+// };
 
 // Render food names to dropdown btns
 
