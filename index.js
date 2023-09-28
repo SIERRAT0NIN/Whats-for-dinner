00a4c23f506303a7048b5ba7d7f977f175d16273
Alexander <command>
2023-09-28 14:44:40 -0700

    change

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Global variables
 
 
- <!-- anne-branch -->
+
 
 const form = document.querySelector('#new-form');
-const apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
+
 const submittedMeal = document.querySelector('#submitted-meal-container');
 const toggleEl = document.querySelector('.dropdown-toggle')
- <!-- anne-branch -->
+
 
 
 const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?a=`;
@@ -21,6 +21,39 @@ const entree = document.querySelector(".entree");
 
 const dropdownLi = document.createElement("li");
 
+//MODAL T#ESTINGGGGGGGGGG START
+
+//image.addEventListener(`click`, (e) => console.log(e.target))
+
+function openModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+document.addEventListener("click", function (event) {
+  if (!event.target.closest(".modal-content")) {
+    closeModal();
+  }
+})
+
+
+
+image.addEventListener(`click`, () => foodImgDisplay(mealData))
+/*
+
+image.addEventListener(`click`, () => foodImgDisplay(mealData))
+
+
+
+
+
+*/
+//MODAL TESTINGGGGGGGG END
 
 
 // Fetch Request
@@ -36,13 +69,13 @@ const fetchApi = () => {
 // When a country is selected, update the selected-country text content with country's name
 
 
- <!-- anne-branch -->
+
 
 
 // Helper functions
 // When a country is selected, update the selected-country text content with country's name
 
- <!-- anne-branch -->
+
 
 // Fetch Request
 
@@ -52,36 +85,37 @@ const fetchApi = () => {
 // need to post new data entered by user into the API and onto the page
 // ? where should the submited data end up on page?
 //  may need to append data to a location on the page
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // if (toggleEl.style.display === 'none' || toggleEl.style.display === '') {
-    //     toggleEl.style.display = 'block';
-    // } else {
-    //     toggleEl.style.display = 'none;'
-    // }
-    const mealData = mealData(form);
-    const mealObj = {};
-    mealData.forEach((value, key) => {
-        mealObj[key] = value;
-    })
-    sendMealToAPI(mealObj)
-    form.reset();
-});
-const sendMealToAPI = (mealObj => {
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(mealObj),
-    })
-    .then(resp => resp.json())
-    .then(mealArr => console.log(mealArr))
-    .then(mealArr => mealArr.map(mealObj))
-    .catch(err => alert('Cannot Save'))
-});
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+// })
+// if (toggleEl.style.display === 'none' || toggleEl.style.display === '') {
+//     toggleEl.style.display = 'block';
+// } else {
+//     toggleEl.style.display = 'none;'
+// }
+//   const mealData = mealData(form);
+//   const mealObj = {};
+//   mealData.forEach((value, key) => {
+//     mealObj[key] = value;
+//   })
+//   sendMealToAPI(mealObj)
+//   form.reset();
+// });
+// const sendMealToAPI = (mealObj => {
+//   fetch(apiUrl, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(mealObj),
+//   })
+//     .then(resp => resp.json())
+//     .then(mealArr => console.log(mealArr))
+//     .then(mealArr => mealArr.map(mealObj))
+//     .catch(err => alert('Cannot Save'))
+// });
 
- <!-- anne-branch -->
+
 
 
 var selectedCountry = "";
@@ -198,7 +232,3 @@ closeModalBtn.addEventListener("mouseover", closeModal);
 
 </d>
 */
-
- <!-- anne-branch -->
-
-
